e2bf07fa72788f2b3c2f66cad612ff06a727c568
Duviel Garcia <duviel7@gmail.com>
2020-01-17 10:06:18 -0500

    Creación de citasReducer, configuración inicial del store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
+// Redux
+import store from './store';
+import { Provider } from 'react-redux';
+
+
 function App() {
   return (
-    <div className="container">
-      <header>
-        <h1 className="text-center">
-          Administrador de Pacientes de Veterinaria
-        </h1>
-      </header>
-      <div className="row mt-3">
-        <div className="col-md-6">
-          Formulario aqui
-        </div>
-        <div className="col-md-6">
-          Listado de citas aqui
+    <Provider store={store}>
+      <div className="container">
+        <header>
+          <h1 className="text-center">
+            Administrador de Pacientes de Veterinaria
+          </h1>
+        </header>
+        <div className="row mt-3">
+          <div className="col-md-6">
+            Formulario aqui
+          </div>
+          <div className="col-md-6">
+            Listado de citas aqui
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
