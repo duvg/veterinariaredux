0204cd4c7896af55678db1cc2aabd996cf53bd85
Duviel Garcia <duviel7@gmail.com>
2020-01-17 09:28:18 -0500

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1 className="text-center">
+          Administrador de Pacientes de Veterinaria
+        </h1>
+      </header>
+      <div className="row mt-3">
+        <div className="col-md-6">
+          Formulario aqui
+        </div>
+        <div className="col-md-6">
+          Listado de citas aqui
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
